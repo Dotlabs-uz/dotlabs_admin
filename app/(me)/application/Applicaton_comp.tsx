b73548f7db858def229c306023b0e74efdd933b5
Mirsaid25 <siddikovmirsaid01@gmail.com>
2024-01-28 14:59:02 +0500

    add table

--- app/(me)/application/Applicaton_comp.tsx
+++ app/(me)/application/Applicaton_comp.tsx
@@ -3,6 +3,15 @@ import OrdersItem from "@/components/OrdersItem";
 import Skleton from "@/components/Skeleton";
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 
 interface Application_compProps {}
 
@@ -38,7 +47,7 @@ const Application_comp: React.FC<Application_compProps> = () => {
 
     useEffect(() => {
         console.log(sourceArchiveOrders);
-        
+
         const arr = sourceArchiveOrders.filter((item: any) => {
             if (item.status === heanleChangeStatus) {
                 return item;
@@ -55,43 +64,67 @@ const Application_comp: React.FC<Application_compProps> = () => {
                     <div className="flex items-center border border-gray-300 rounded-md overflow-hidden w-fit mb-3 text-sm bg-white">
                         <button
                             onClick={() => setHeanleChangeStatus("new")}
-                            className={`${heanleChangeStatus === "new"? "bg-[#23ABF2] text-white":null} hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
+                            className={`${
+                                heanleChangeStatus === "new"
+                                    ? "bg-[#23ABF2] text-white"
+                                    : null
+                            } hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
                         >
                             Новые
                         </button>
                         <button
                             onClick={() => setHeanleChangeStatus("inProgress")}
-                            className={`${heanleChangeStatus === "inProgress"? "bg-[#23ABF2] text-white":null} hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
+                            className={`${
+                                heanleChangeStatus === "inProgress"
+                                    ? "bg-[#23ABF2] text-white"
+                                    : null
+                            } hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
                         >
                             В процессе
                         </button>
                         <button
                             onClick={() => setHeanleChangeStatus("pending")}
-                            className={`${heanleChangeStatus === "pending"? "bg-[#23ABF2] text-white":null} hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
+                            className={`${
+                                heanleChangeStatus === "pending"
+                                    ? "bg-[#23ABF2] text-white"
+                                    : null
+                            } hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
                         >
                             Ожидающие
                         </button>
                         <button
                             onClick={() => setHeanleChangeStatus("noResponse")}
-                            className={`${heanleChangeStatus === "noResponse"? "bg-[#23ABF2] text-white":null} hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
+                            className={`${
+                                heanleChangeStatus === "noResponse"
+                                    ? "bg-[#23ABF2] text-white"
+                                    : null
+                            } hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
                         >
                             Не ответили
                         </button>
                         <button
                             onClick={() => setHeanleChangeStatus("closed")}
-                            className={`${heanleChangeStatus === "closed"? "bg-[#23ABF2] text-white":null} hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
+                            className={`${
+                                heanleChangeStatus === "closed"
+                                    ? "bg-[#23ABF2] text-white"
+                                    : null
+                            } hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6 border-r border-gray-300`}
                         >
                             Закрытые
                         </button>
                         <button
                             onClick={() => setHeanleChangeStatus("declined")}
-                            className={`${heanleChangeStatus === "declined"? "bg-[#23ABF2] text-white":null} hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6`}
+                            className={`${
+                                heanleChangeStatus === "declined"
+                                    ? "bg-[#23ABF2] text-white"
+                                    : null
+                            } hover:bg-[#23ABF2] hover:text-white transition-all py-[6px] px-6`}
                         >
                             Отказы
                         </button>
                     </div>
                     <div className="overflow-x-auto shadow-md sm:rounded-lg min-h-[80vh] relative pb-[80px]">
-                        <div className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        {/* <div className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             {archiveOrders.map((item: any, key: any) => (
                                 <OrdersItem key={key} item={item} idx={key} />
                             ))}
@@ -101,7 +134,48 @@ const Application_comp: React.FC<Application_compProps> = () => {
                                     Ничего не найдено
                                 </div>
                             )}
-                        </div>
+                        </div> */}
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[50px]">
+                                        №
+                                    </TableHead>
+                                    <TableHead className="text-center">
+                                        Имя
+                                    </TableHead>
+                                    <TableHead className="text-center">
+                                        Номер
+                                    </TableHead>
+                                    <TableHead className="text-center">
+                                        Статус
+                                    </TableHead>
+                                    <TableHead className="text-center">
+                                        Дата
+                                    </TableHead>
+                                    <TableHead className="text-center">
+                                        Описание
+                                    </TableHead>
+                                    <TableHead className="text-end">
+                                        Изменение
+                                    </TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {archiveOrders.map((item: any, key: any) => (
+                                    <OrdersItem
+                                        key={key}
+                                        item={item}
+                                        idx={key}
+                                    />
+                                ))}
+                            </TableBody>
+                        </Table>
+                        {archiveOrders.length === 0 && (
+                            <div className="w-full flex items-center justify-center text-sm mt-5">
+                                Ничего не найдено {":("}
+                            </div>
+                        )}
                     </div>
                 </>
             ) : (
--- components/OrdersItem.tsx
+++ components/OrdersItem.tsx
@@ -8,10 +8,22 @@ import {
     DropdownMenuContent,
     DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { TableCell, TableRow } from "./ui/table";
+import { GoPencil } from "react-icons/go";
+import moment from"moment";
 
 function OrdersItem({ item, idx }: any) {
     const [cookies] = useCookies(["token"]);
     const [loading, setLoading] = useState(false);
+
+    const date = moment(item.createdAt).subtract(10, 'days').calendar();
+    
 
     function ChangeStatus(stat: string) {
         setLoading(true);
@@ -37,68 +49,159 @@ function OrdersItem({ item, idx }: any) {
     }
 
     return (
-        <div
-            className={`flex items-center justify-between py-3 pr-5 pl-3 border-b border-b-[#cccccc]`}
-        >
-            <div className="flex items-start gap-3">
-                <span>{idx + 1}.</span>
-                <div>
-                    <p className="font-medium">{item?.name}</p>
-                    <a href={`tel:+${item?.phone}`} className="mt-1">
-                        +{item?.phone}
-                    </a>
+        <>
+            <TableRow>
+                <TableCell className="font-medium">{idx + 1}</TableCell>
+                <TableCell className="text-center">{item?.name}</TableCell>
+                <TableCell className="text-center">
+                    <a href={`tel:+${item?.phone}`}>+{item?.phone}</a>
+                </TableCell>
+                <TableCell className="text-center">{item?.status}</TableCell>
+                <TableCell className="text-center">
+                    {date}
+                </TableCell>
+                <TableCell className="text-center relative">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className={"outline-none"}>
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <GoPencil
+                                            size={19}
+                                            className="cursor-pointer"
+                                        />
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Add to library</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            className={"-translate-y-1/2 p-3 text-sm"}
+                        >
+                            <form className="flex flex-col gap-3">
+                                <textarea
+                                    placeholder="Type your message here."
+                                    className="border rounded-sm p-2 h-[100px]"
+                                ></textarea>
+                                <Button className="bg-[#2387f2] hover:bg-[#23ABF2]">
+                                    Submit
+                                </Button>
+                            </form>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </TableCell>
+                <TableCell className="relative text-end">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className={"outline-none"}>
+                            <BsThreeDots size={25} className="cursor-pointer" />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            className={"flex flex-col -ml-16 -mt-11 gap-1"}
+                        >
+                            <Button
+                                onClick={(e: any) => ChangeStatus("inProgress")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                В процессе
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("pending")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                Ждём ответа
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("closed")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                Получили заказ
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("noResponse")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                Не ответил
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("declined")}
+                                variant={"destructive"}
+                                disabled={loading}
+                            >
+                                Отказ
+                            </Button>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </TableCell>
+            </TableRow>
+            {/* <div
+                className={`flex items-center justify-between py-3 pr-5 pl-3 border-b border-b-[#cccccc]`}
+            >
+                <div className="flex items-start gap-3">
+                    <span>{idx + 1}.</span>
+                    <div>
+                        <p className="font-medium">{item?.name}</p>
+                        <a href={`tel:+${item?.phone}`} className="mt-1">
+                            +{item?.phone}
+                        </a>
+                    </div>
                 </div>
-            </div>
 
-            <p>{item?.status}</p>
+                <p>{item?.status}</p>
 
-            <div className="relative">
-                <DropdownMenu>
-                    <DropdownMenuTrigger className={"outline-none"}>
-                        <BsThreeDots size={25} className="cursor-pointer" />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent
-                        className={"flex flex-col -ml-16 -mt-11 gap-1"}
-                    >
-                        <Button
-                            onClick={(e: any) => ChangeStatus("inProgress")}
-                            disabled={loading}
-                            className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                <div className="relative">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className={"outline-none"}>
+                            <BsThreeDots size={25} className="cursor-pointer" />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            className={"flex flex-col -ml-16 -mt-11 gap-1"}
                         >
-                            В процессе
-                        </Button>
-                        <Button
-                            onClick={() => ChangeStatus("pending")}
-                            disabled={loading}
-                            className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
-                        >
-                            Ждём ответа
-                        </Button>
-                        <Button
-                            onClick={() => ChangeStatus("closed")}
-                            disabled={loading}
-                            className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
-                        >
-                            Получили заказ
-                        </Button>
-                        <Button
-                            onClick={() => ChangeStatus("noResponse")}
-                            disabled={loading}
-                            className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
-                        >
-                            Не ответил
-                        </Button>
-                        <Button
-                            onClick={() => ChangeStatus("declined")}
-                            variant={"destructive"}
-                            disabled={loading}
-                        >
-                            Отказ
-                        </Button>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-        </div>
+                            <Button
+                                onClick={(e: any) => ChangeStatus("inProgress")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                В процессе
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("pending")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                Ждём ответа
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("closed")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                Получили заказ
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("noResponse")}
+                                disabled={loading}
+                                className={"bg-[#2387f2] hover:bg-[#23ABF2]"}
+                            >
+                                Не ответил
+                            </Button>
+                            <Button
+                                onClick={() => ChangeStatus("declined")}
+                                variant={"destructive"}
+                                disabled={loading}
+                            >
+                                Отказ
+                            </Button>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </div> */}
+        </>
     );
 }
 
